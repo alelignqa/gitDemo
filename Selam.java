d46cf4e5204aa469ae157a8505426b6e67665b56
alelignqa <alent.qa@gmail.com>
2021-01-10 06:17:46 -0500

    this first java code pushed to git

---
+++ Selam.java
@@ -0,0 +1,18 @@
+public class Selam {
+
+	  String firstName="John";
+	  String lastName="Kello";
+	  int age=20;
+	  double length=6.2;
+
+	public static void main(String[] args){
+	
+	Selam obj=new Selam();
+
+
+		System.out.println("Peace is very important comodity that humans failed to keep untill we lost");
+
+		System.out.println("The boy name is " + obj.firstName + " " + obj.lastName + " he is " + obj.age + " years age and " + obj.length + " long ");
+	}
+
+}
